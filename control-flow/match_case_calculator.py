317f97d7b79f32dd414c7fd4fb86196b5bacd58b
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2024-12-21 17:46:29 +0200

    cal some operations math

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,18 +3,20 @@ num2 = int(input("Enter the second number:"))
 
 operation = input("Choose the operation (+, -, *, /):")
 
-def cal(num1,num2,operation):
-    if operation=="+":
-        return num1+num2
-    elif operation=="-":
-        return num1-num2
-    elif operation=="*":
-        return num1*num2
-    elif operation =="/":
-        if num2 == 0:
-            return "error"
-        else:
-            return num1/num2
+def cal(num1, num2, operation):
+    match operation:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            if num2 == 0:
+                return "error"
+            else:
+                return num1 / num2
+
 
 out=cal(num1,num2,operation)
 if out=="error":
