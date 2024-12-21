0f0289cb866b37207731e82ee4820af71ba0eb42
Mostafa Emad <mostafa.3mad.salah@gmail.com>
2024-12-21 17:41:54 +0200

    cal some operations math

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+operation = input("Choose the operation (+, -, *, /):")
+
+def cal(num1,num2,operation):
+    if operation=="+":
+        return num1+num2
+    elif operation=="-":
+        return num1-num2
+    elif operation=="*":
+        return num1*num2
+    elif operation =="/":
+        if num2 == 0:
+            return "error"
+        else:
+            return num1/num2
+
+out=cal(num1,num2,operation)
+if out=="error":
+    print("Cannot divide by zero.")
+else:
+    print(f"The result is {out}")
